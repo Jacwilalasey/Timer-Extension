b2e959bc1bebd228bb112337fb2b9a2c47039339
Jacob Seymour <jacobwseymour@gmail.com>
2023-06-19 21:17:47 +0100

    added basic timer functionality, working on design and stop/start functions

---
+++ popup.js
@@ -0,0 +1,18 @@
+const time = document.getElementById("time");
+const nameElement = document.getElementById("name");
+const currentTime = new Date().toLocaleTimeString();
+
+time.textContent = `The time is: ${currentTime}`;
+
+chrome.action.setBadgeText(
+  {
+    text: "TIME",
+  },
+  () => {
+    console.log("Finished setting badge text.");
+  }
+);
+
+chrome.storage.sync.get(["name"], (res) => {
+  nameElement.textContent = `Your name is ${res.name}`;
+});
